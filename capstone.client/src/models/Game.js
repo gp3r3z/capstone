b7bc7c08d33ecfee5242e8816eea9285661584b7
Gillardo <gilperez109@gmail.com>
2022-12-22 09:36:28 -0700

    updated model

--- capstone.client/src/models/Game.js
+++ capstone.client/src/models/Game.js
@@ -7,6 +7,10 @@ export class Game {
         this.ratings = data.ratings.reviews_count
         this.name = data.name
         this.id = data.id
+        this.rating = data.rating
+        this.screenshots = data.short_screenshots
+        this.stores = data.stores
+        this.addedStatus = data.added_by_status
 
 
 
