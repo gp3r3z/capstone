68afb22c1f19032bacfb378f9f72db189456325a
Gillardo <gilperez109@gmail.com>
2023-01-10 20:45:57 -0700

    updated model to include cover image for account

--- capstone/server/models/Account.js
+++ capstone/server/models/Account.js
@@ -10,6 +10,7 @@ export const AccountSchema = new Schema(
     bio: { type: String },
     gamerTags: { type: Schema.Types.Array },
     skillLevel: { type: String, default: 'begginer', enum: ['beginner', 'casual', 'competitive'], lowercase: true },
+    coverImage: { type: String },
 
     // NOTE If you wish to add additional properties do so here
   },
